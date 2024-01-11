432a2105f7fe0ae4653ccbd33a8ffe792cbf3f9d
cjh1249131356 <cjh1249131356@gmail.com>
2024-01-11 16:04:47 +0000

    soong: Add TARGET_USES_OPLUS_CAMERA
    Change-Id: Ifb3ff3a1141712a255640676d24b018ae516add8
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -51,7 +51,8 @@ SOONG_CONFIG_apexGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_egl_display_array \
-    uses_legacy_fd_fbdev
+    uses_legacy_fd_fbdev \
+    uses_oplus_camera
 
 SOONG_CONFIG_NAMESPACES += apexNvidiaVars
 SOONG_CONFIG_apexNvidiaVars += \
@@ -82,6 +83,7 @@ SOONG_CONFIG_apexQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATO
 SOONG_CONFIG_apexQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_apexQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_apexGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
+SOONG_CONFIG_apexGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
